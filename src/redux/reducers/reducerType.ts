a458fd36ac19a3c996af33fc29448bb9a612515a
anhtuanpro333 <anhtuan03.MDev@gmail.com>
2024-09-25 09:39:06 +0700

    Merge after pull

--- src/redux/reducers/reducerType.ts
+++ src/redux/reducers/reducerType.ts
@@ -1,17 +1,18 @@
-import {AlphaColorTheme, ColorTheme} from '../../themes/types';
-
-export type TActionProp<T> = {
-  type: string;
-  payload?: T;
-};
-
-export type TGlobalState = 
-{
-  load: boolean;
-  colorScheme: ColorTheme;
-  colorSchemeAlpha: AlphaColorTheme;
-  success: boolean,
-  error: boolean,
-  successMess: string,
-  errorMess: string
-};
+    import {AlphaColorTheme, ColorTheme} from '../../themes/types';
+    
+    export type TActionProp<T> = {
+      type: string;
+      payload?: T;
+    };
+    
+    export type TGlobalState = 
+    {
+      load: boolean;
+      colorScheme: ColorTheme;
+      colorSchemeAlpha: AlphaColorTheme;
+      success: boolean,
+      error: boolean,
+      successMess: string,
+      errorMess: string
+    };
+    
